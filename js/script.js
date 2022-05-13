85b226fa55ac57b8ca41e96082c9d05a8e3ade5e
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-13 14:23:23 -0300

    add animals and countrys for secret words

---
+++ js/script.js
@@ -0,0 +1,82 @@
+const words = [
+    wd001= {
+        name: "BRASIL",
+        category: "COUNTRYS"
+    },
+    wd002= {
+        name:"UNITED STATES",
+        category: "COUNTRYS"
+    },
+    wd003= {
+        name:"CANADA",
+        category: "COUNTRYS"
+    },
+    wd004= {
+        name:"JAPAN",
+        category: "COUNTRYS"
+    },
+    wd005= {
+        name:"ENGLAND",
+        category: "COUNTRYS"
+    },
+    wd006= {
+        name:"CHINA",
+        category: "COUNTRYS"
+    },
+    wd007= {
+        name:"EUROPE",
+        category: "COUNTRYS"
+    },
+    wd008= {
+        name:"PORTUGAL",
+        category: "COUNTRYS"
+    },
+    wd009= {
+        name:"CUBA",
+        category: "COUNTRYS"
+    },
+    wd010= {
+        name:"CHILE",
+        category: "COUNTRYS"
+    },
+    wd011= {
+        name:"DOG",
+        category: "ANIMALS"
+    },
+    wd012= {
+        name:"CAT",
+        category: "ANIMALS"
+    },
+    wd013= {
+        name:"BAT",
+        category: "ANIMALS"
+    },
+    wd014= {
+        name:"FOX",
+        category: "ANIMALS"
+    },
+    wd015= {
+        name:"FISH",
+        category: "ANIMALS"
+    },
+    wd016= {
+        name:"BULL",
+        category: "ANIMALS"
+    },
+    wd017= {
+        name:"PIG",
+        category: "ANIMALS"
+    },
+    wd018= {
+        name:"GIRAFFE",
+        category: "ANIMALS"
+    },
+    wd019= {
+        name:"OWL",
+        category: "ANIMALS"
+    },
+    wd020= {
+        name:"TIGER",
+        category: "ANIMALS"
+    }
+]
